ca0ef4bcb5c810ebc50ae24e7463dbfaedc969f5
0teklee <leetekwoo@gmail.com>
2022-09-27 15:19:01 +0900

    fix: modal close when  click outside

--- src/components/Atom/Modal.tsx
+++ src/components/Atom/Modal.tsx
@@ -1,7 +1,32 @@
-import React, { ReactNode } from "react";
+import { Dispatch, ReactNode, SetStateAction, useRef } from "react";
 
-const Modal = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+const Modal = ({
+  children,
+  setState,
+}: {
+  children: ReactNode;
+  setState: Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const modalRef = useRef(null);
+
+  return (
+    <div
+      ref={modalRef}
+      className="fixed top-0 left-0 w-screen h-screen bg-gray-700 z-40 bg-opacity-40 overscroll-y-none"
+      onClick={(e) => {
+        setState(false);
+      }}
+    >
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        className="fixed transform -translate-y-1/2 -translate-x-1/2 top-1/2 left-1/2  z-50 p-20 rounded-lg bg-white text-black"
+      >
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
