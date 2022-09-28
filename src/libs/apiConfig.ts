78bfd2c7d6c97c88f2ae05587384c2c6a5d29e1c
0teklee <leetekwoo@gmail.com>
2022-09-29 07:45:53 +0900

    feat: added api config keys

---
+++ src/libs/apiConfig.ts
@@ -0,0 +1,9 @@
+const clientId =
+  "707048241800-hvjbu1v5ip510h93bpsm5r81qk3fpvpn.apps.googleusercontent.com";
+
+const calendarId =
+  "97718e07a64b773650fd4914e995c35883f5f6d32a2f3a436bbe4bec75800df8@group.calendar.google.com";
+
+const apiKey = "AIzaSyDDMGUHw7F0Q4NMeG12y77T-sZIIUcvR18";
+
+export { clientId, calendarId, apiKey };
