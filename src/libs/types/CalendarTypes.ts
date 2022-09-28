916365f6e65424de6492eb905df6f7d5d7bd21c5
0teklee <leetekwoo@gmail.com>
2022-09-29 06:31:48 +0900

    fix: refactored types

--- src/libs/types/CalendarTypes.ts
+++ src/libs/types/CalendarTypes.ts
@@ -1,17 +1,38 @@
-import { ViewApi } from "@fullcalendar/react";
-
 interface IEventDetail {
   title: string;
   description?: string;
+  location?: string;
   start: Date | null;
   end: Date | null;
-  location?: string;
 }
 
 interface ISelectDate {
-  allDay?: boolean;
-  end?: Date;
-  start?: Date;
+  start: Date;
+  end: Date;
 }
 
-export type { IEventDetail, ISelectDate };
+interface IPostEventBody extends Omit<IEventDetail, "start" | "end"> {
+  title: string;
+  description?: string;
+  location?: string;
+  start: Date;
+  end: Date;
+}
+
+interface ICreateProps extends Omit<IPostEventBody, "start" | "end"> {
+  start: string;
+  end: string;
+}
+
+interface IGapiInsert extends Omit<IPostEventBody, "start" | "end" | "title"> {
+  start: { dateTime: string; timeZone: string };
+  end: { dateTime: string; timeZone: string };
+}
+
+export type {
+  IEventDetail,
+  ISelectDate,
+  IPostEventBody,
+  IGapiInsert,
+  ICreateProps,
+};
