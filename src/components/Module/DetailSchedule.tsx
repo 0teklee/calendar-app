614d30cc6c592fdefcd94e2f387ac0b833ec4709
0teklee <leetekwoo@gmail.com>
2022-09-29 07:48:26 +0900

    chore: changed date format

--- src/components/Module/DetailSchedule.tsx
+++ src/components/Module/DetailSchedule.tsx
@@ -13,42 +13,44 @@ const DetailSchedule = ({
 
   return (
     <Modal setState={setState}>
-      <div className="w-80 mb-4">
-        <h1 className="inline mr-5 font-bold">제목 :</h1>
-        <span>{title}</span>
-      </div>
-      <div className="w-80 mb-4">
-        <h1 className="inline mr-5 font-bold">시작 :</h1>
-        <span>
-          {start?.toLocaleString("ko-KR", {
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-          })}
-        </span>
-      </div>
-      <div className="w-80 mb-4">
-        <h1 className="inline mr-5 font-bold">종료 :</h1>
-        <span>
-          {end?.toLocaleString("ko-KR", {
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-          })}
-        </span>
-      </div>
-      <div className="w-80 mb-4">
-        <h2 className="inline mr-5 font-bold">장소 :</h2>
-        <span>{location ? location : "장소 없음"}</span>
-      </div>
+      <div className="p-12">
+        <div className="w-80 mb-4">
+          <h1 className="inline mr-5 font-bold">제목 :</h1>
+          <span>{title}</span>
+        </div>
+        <div className="w-80 mb-4">
+          <h1 className="inline mr-5 font-bold">시작 :</h1>
+          <span>
+            {start?.toLocaleString("ko-KR", {
+              year: "numeric",
+              month: "short",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+          </span>
+        </div>
+        <div className="w-80 mb-4">
+          <h1 className="inline mr-5 font-bold">종료 :</h1>
+          <span>
+            {end?.toLocaleString("ko-KR", {
+              year: "numeric",
+              month: "short",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+          </span>
+        </div>
+        <div className="w-80 mb-4">
+          <h2 className="inline mr-5 font-bold">장소 :</h2>
+          <span>{location ? location : "장소 없음"}</span>
+        </div>
 
-      <div className="w-80 mb-4">
-        <h2 className="mb-4 font-bold">설명 :</h2>
-        <p>{description}</p>
+        <div className="w-80 mb-4">
+          <h2 className="mb-4 font-bold">설명 :</h2>
+          <p>{description}</p>
+        </div>
       </div>
     </Modal>
   );
